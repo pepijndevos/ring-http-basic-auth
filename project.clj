2863bae28cac629578c967cd3dc47d744d6be726
Pepijn de Vos <pepijndevos@gmail.com>
2011-11-16 15:44:08 +0100

    version push

--- project.clj
+++ project.clj
@@ -1,3 +1,3 @@
-(defproject ring-http-basic-auth "0.0.1"
+(defproject ring-http-basic-auth "0.1.0"
   :description "Ring middleware for basic HTTP authentication."
   :dependencies [[org.clojure/clojure "1.3.0"]])
